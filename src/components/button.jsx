a898a9a19d200b679b09b9bb2d1d70cb8f3e57e3
Nishant804-alt <nishant.s85@kalvium.community>
2025-03-12 10:50:45 +0530

    completed the task

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,12 @@
 // write button card here
+// write button card here
+import React from "react";
+
+const Button=({label,onClick})=>{
+    return(
+        <button className="viewproduct button" onClick={onClick}>{label}</button>
+    )
+}
+
+
+export default Button;
